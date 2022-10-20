4d92a36e225d8a2133cc88c2b9cf0610e53afeda
Manali <bodarmanali66@gmail.com>
2022-10-20 10:56:01 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// bring ratings class ,button,ratings container and panel
+
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.rating-container')
+const panel = document.querySelector('#panel')
+const sendBtn = document.querySelector('#send')
+
+let selectRatings = 'Satisfied'
+
+// add event listener on rating container
+ratingsContainer.addEventListener('click',(e) =>{
+    if(e.target.parentNode.classList.contains('rating'))
+    {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectRatings = e.target.nextElementSibling.innerHTML
+        console.log(selectRatings)
+    }
+})
+
+// add click event listener on button
+sendBtn.addEventListener('click' ,(e) => {
+    panel.innerHTML = `
+    <i class="fa fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>FeedBack:${selectRatings}</strong>
+    <p>we'll use your feedback to improve our customer support</p>
+    `
+})
+
+
+// create function removeActive
+
+function removeActive()
+{
+    for(let i = 0 ; i< ratings.length ; i++)
+    {
+        ratings[i].classList.remove('active')
+    }
+}
